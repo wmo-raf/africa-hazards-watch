03527ccb50c88834b3875837ea63875721d83b65
Erick Otenyo <otenyo.erick@gmail.com>
2023-02-02 12:13:29 +0300

    update globe img

--- layouts/home/home-globe.jsx
+++ layouts/home/home-globe.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ahwGlobeBg from "./assets/earth-blue-marble-min.jpg";
+import africaCountries from "./assets/countries.json";
 
 let Globe = () => null;
 if (typeof window !== "undefined") Globe = require("react-globe.gl").default;
@@ -9,18 +10,18 @@ export default class HomeGlobe extends Component {
     super(props);
     // create a ref to store the textInput DOM element
     this.globeRef = React.createRef();
-  }
 
-  componentDidMount() {
-    // fetch("../datasets/ne_110m_admin_0_countries.geojson")
-    //   .then((res) => res.json())
-    //   .then(setCountries);
+    this.state = {
+      countries: null,
+    };
   }
 
   onGlobeReady = () => {
+    this.globeRef.current.scene().position.y = 20;
+
     this.globeRef.current.controls().enableZoom = false;
     this.globeRef.current.controls().autoRotate = true;
-    this.globeRef.current.controls().autoRotateSpeed = 1;
+    this.globeRef.current.controls().autoRotateSpeed = 0.4;
   };
 
   render() {
@@ -33,6 +34,13 @@ export default class HomeGlobe extends Component {
           width={400}
           backgroundColor="#fff"
           globeImageUrl={ahwGlobeBg}
+          atmosphereAltitude={0}
+          polygonsData={africaCountries.features}
+          polygonCapColor={() => "rgba(255, 255, 255, 0)"}
+          polygonSideColor={() => "rgba(164, 219, 253, 0.5)"}
+          polygonLabel={({ properties: d }) => `
+        <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
+      `}
         />
       </div>
     );
