788c5d7b571b5eafd633fb9dcbbf224610f6b99c
Erick Otenyo <otenyo.erick@gmail.com>
2023-03-02 22:52:42 +0300

    Initial location picker implementation

--- components/map/components/popup/components/boundary-sentence/component.jsx
+++ components/map/components/popup/components/boundary-sentence/component.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import cx from "classnames";
 
-import DynamicSentence from 'components/ui/dynamic-sentence';
-import Button from 'components/ui/button';
+import DynamicSentence from "components/ui/dynamic-sentence";
+import Button from "components/ui/button";
 
-import './styles.scss';
+import "./styles.scss";
 
-const BoundarySentence = ({ sentence, onAnalyze }) => (
+const BoundarySentence = ({ sentence, onAnalyze, actionLabel, small }) => (
   <div className="c-boundary-sentence">
     <DynamicSentence className="sentence" sentence={sentence} />
-    <Button onClick={onAnalyze}>analyze</Button>
+    <Button theme={cx({ "theme-button-medium": small })} onClick={onAnalyze}>
+      {actionLabel ? actionLabel : "analyze"}
+    </Button>
   </div>
 );
 
